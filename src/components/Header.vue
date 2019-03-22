9a2bbffd33e5f988df4044cf149560aaf7d7e89e
Andy Million <andy@summit-education.com>
2019-03-22 12:27:45 -0500

    WIP, getting API data back

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <b-nav tabs>
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+
+  </b-nav>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+        <template slot="header">Bootstrap Vue</template>
+
+        <template slot="lead">
+        question
+        </template>
+
+        <hr class="my-4" />
+
+        <p>
+        list of answers
+        </p>
+
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
